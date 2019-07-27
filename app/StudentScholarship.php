14b80ae059c4a54eb43ea9b838b4f773a27db6d3
Rohin Vaidya <rohinvaidya@users.noreply.github.com>
2019-07-27 17:37:10 +0530

    Update StudentScholarship.php

--- app/StudentScholarship.php
+++ app/StudentScholarship.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StudentScholarship extends Model
 {
+    use SoftDeletes;
     protected $table = 'student_scholarships';
     protected $primaryKey = 'id';
     protected $guarded = [];
