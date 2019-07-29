80eeadd07bba2fe7dd0706cfea213a71a8852fae
Pushpak Chhajed <pushpak1300@gmail.com>
2019-07-29 20:29:58 +0530

    Dashboard Rectification

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -146,10 +146,10 @@
                     <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <div class="media align-items-center">
                   <span class="avatar avatar-sm rounded-circle">
-                    <img alt="Image placeholder" src="{{ asset('assets/img/theme/team-4.jpg') }}">
+                    <img alt="Image placeholder" src="{{ Auth::user()->photo }}">
                   </span>
                             <div class="media-body ml-2 d-none d-lg-block">
-                                <span class="mb-0 text-sm  font-weight-bold">John Snow</span>
+                            <span class="mb-0 text-sm  font-weight-bold">{{Auth::user()->first_name}}</span>
                             </div>
                         </div>
                     </a>
--- resources/views/dashboard/staff.blade.php
+++ resources/views/dashboard/staff.blade.php
@@ -1,7 +1,87 @@
 @extends('layouts.base')
 
 @section('page-content')
-    
+    <div class="container-fluid">
+        
+          <!-- Card stats -->
+          <div class="row">
+            <div class="col-xl-4 col-md-6">
+              <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">IPR</h5>
+                    <span class="h2 font-weight-bold mb-0">{{$user->staff->ipr->count()}}</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-sm">
+                      <a href="/ipr/create">
+                    <span class="text-success mr-2"><i class="ni ni-fat-add"></i> </span>
+                    <span class="text-nowrap">Add IPR</span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 col-md-6">
+              <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Events</h5>
+                    <span class="h2 font-weight-bold mb-0">{{$user->staff->events->count()}}</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                        <i class="ni ni-calendar-grid-58"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-sm">
+                      <a href="/events/create">
+                    <span class="text-success mr-2"> </span>
+                    <span class="text-nowrap"></span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-xl-4 col-md-6">
+              <div class="card card-stats">
+                <!-- Card body -->
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">Research Project</h5>
+                    <span class="h2 font-weight-bold mb-0">{{ count($user->staff->researchProjects->all())}}</span>
+                    </div>
+                    <div class="col-auto">
+                      <div class="icon icon-shape bg-gradient-blue text-white rounded-circle shadow">
+                        <i class="ni ni-spaceship"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="mt-3 mb-0 text-sm">
+                      <a href="/research-projects/create">
+                    <span class="text-success mr-2"><i class="ni ni-fat-add"></i> </span>
+                    <span class="text-nowrap">Add Reasearch Project</span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            
+              </div>
+            </div>
+         
 @endsection
 
 @section('custom-script')
